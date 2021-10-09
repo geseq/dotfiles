436b42d8b129d5d5da5c1b4973c9bcfe09c086e9
Earncef Sequeira <earncef@earncef.com>
2021-10-09 20:13:47 +0100

    improved zshrc

--- shell.sh
+++ shell.sh
@@ -1,23 +1,57 @@
 echo "Backing up ~/.zshrc to ~/.zshrc.bak"
+rm -f $HOME/.zshrc.bak
 mv $HOME/.zshrc $HOME/.zshrc.bak
+
+rm -rf $HOME/.zsh.bak
+mv $HOME/.zsh $HOME/.zsh.bak
 
 # Configure terminal
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git $HOME/.zsh/themes/powerlevel10k
 
-echo "source $HOME/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> $HOME/.zshrc
-echo "source $HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> $HOME/.zshrc
-echo "source $HOME/.zsh/themes/powerlevel10k/powerlevel10k.zsh-theme" >> $HOME/.zshrc
-echo "ZSH_AUTOSUGGEST_STRATEGY=(history completion)" >> $HOME/.zshrc
-echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> $HOME/.zshrc
-echo "export HOMEBREW_NO_INSECURE_REDIRECT=1" >> $HOME/.zshrc
-echo "export HOMEBREW_CASK_OPTS=--require-sha" >> $HOME/.zshrc
-echo "alias zr='source $HOME/.zshrc'" >> $HOME/.zshrc
-echo "alias lg='lazygit'" >> $HOME/.zshrc
-echo 'ds() { docker stop $(docker ps -a -q); }' >> $HOME/.zshrc
-echo 'drm() { docker rm $(docker ps -a -q); }' >> $HOME/.zshrc
-echo 'drmi() { docker rmi $(docker images -q) -f; }' >> $HOME/.zshrc
-echo 'drmv() { docker volume prune; }' >> $HOME/.zshrc
+cat >> $HOME/.zshrc <<"EOT"
+source $HOME/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $HOME/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOME/.zsh/themes/powerlevel10k/powerlevel10k.zsh-theme
+
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+export ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+export ZSH_THEME="powerlevel10k/powerlevel10k"
+export HOMEBREW_NO_INSECURE_REDIRECT=1
+export HOMEBREW_CASK_OPTS=--require-sha
+
+#go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin
+
+# set default editor
+export EDITOR=vim
+
+# set history size
+export HISTSIZE=10000
+# save history after logout
+export SAVEHIST=10000
+# history file
+export HISTFILE=~/.zhistory
+# append into history file
+setopt INC_APPEND_HISTORY
+# save only one command if 2 common are same and consistent
+setopt HIST_IGNORE_DUPS
+# add timestamp for each entry
+setopt EXTENDED_HISTORY   
+
+# aliases
+alias zr='source $HOME/.zshrc'
+alias lg='lazygit'
+
+# functions
+ds() { docker stop $(docker ps -a -q);  }
+drm() { docker rm $(docker ps -a -q);  }
+drmi() { docker rmi $(docker images -q) -f;  }
+drmv() { docker volume prune;  }
+
+EOT
 
 chsh -s $(which zsh)
