095de0825f62b7c11ce9968e16321fd6ef23707e
E Sequeira <earncef@earncef.com>
2022-07-03 13:49:02 +0100

    remove tilix

--- scratch.sh
+++ scratch.sh
@@ -10,7 +10,6 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt -y purge apport
 sudo apt -y purge cloud-init
 sudo apt -y purge lxd-agent-loader
-sudo apt -y purge byobu
 sudo rm -rf /etc/cloud && sudo rm -rf /var/lib/cloud
 
 # Update and install necessary components
@@ -116,5 +115,8 @@ echo "network:
 
 sudo netplan generate
 sudo netplan apply
+
+# More cleanup
+sudo apt -y purge byobu tilix
 
 reboot
