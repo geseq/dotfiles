0cb1bc974939f587c5db051201e0f38e0aec852d
E Sequeira <earncef@earncef.com>
2022-08-13 16:46:16 +0100

    add brightness controls (#34)
    * add brightness controls

--- scratch.sh
+++ scratch.sh
@@ -84,6 +84,10 @@ sudo add-apt-repository -y ppa:agornostal/ulauncher
 sudo apt update && sudo apt install -y ulauncher
 systemctl --user enable --now ulauncher
 
+# light
+sudo apt install light
+sudo chmod +s /usr/bin/light
+
 # grim and slurp for screenshots in sway
 sudo apt install -y grim slurp
 
